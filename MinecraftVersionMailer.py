e2a85b033322e73b57417fa3753d83583c8db145
JMDirksen <jeftadirksen@gmail.com>
2024-12-01 11:32:38 +0100

    send_mail

--- MinecraftVersionMailer.py
+++ MinecraftVersionMailer.py
@@ -1,7 +1,11 @@
 import json
-from urllib.request import urlopen
-from urllib.request import Request
+from urllib.request import urlopen, Request
 import re
+from email.message import EmailMessage
+import smtplib
+import ssl
+import zlib
+from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
 
 
 def main():
@@ -20,6 +24,39 @@ def process_variant_type(variant, type):
     if version and version["id"] not in db[variant][type]["versions"]:
         db[variant][type]["versions"] += [version["id"]]
         db[variant][type]["latest"] = version["id"]
+        subject = f"New Minecraft {variant.capitalize()} {type} version {
+            version["id"]}"
+        message = f"Minecraft {variant.capitalize()} {type} version {
+            version["id"]} has been released."
+        send_mail(subject, message)
+
+
+def send_mail(subject, message):
+    server = db["config"]["mail_server"]
+    port = db["config"]["mail_port"]
+    context = ssl.create_default_context()
+    username = db["config"]["mail_username"]
+    password = unobscure(db["config"]["mail_password"])
+    from_address = db["config"]["mail_from"]
+    to_address = db["config"]["mail_to"]
+
+    msg = EmailMessage()
+    msg["From"] = from_address
+    msg["To"] = to_address
+    msg["Subject"] = subject
+    msg.set_content(message)
+
+    with smtplib.SMTP_SSL(server, port, context=context) as smtp:
+        smtp.login(username, password)
+        smtp.send_message(msg)
+
+
+def obscure(data: str) -> str:
+    return b64e(zlib.compress(data.encode(), 9)).decode()
+
+
+def unobscure(obscured: str) -> str:
+    return zlib.decompress(b64d(obscured.encode())).decode()
 
 
 def loadDb():
@@ -27,10 +64,32 @@ def loadDb():
         with open("db.json", "r") as f:
             return json.load(f)
     except FileNotFoundError:
+        mail_server = input("Mailserver (Like: smtp.gmail.com): ")
+        mail_port = input("Mailserver port (Like: 465): ")
+        mail_username = input("Mail username (Like: user@gmail.com): ")
+        mail_password = obscure(
+            input("Mail password (Google App password?): "))
+        mail_from = input(
+            "Mail from address (Like: Recipient name <recipient@gmail.com>): ")
+        mail_to = input("Mail to address (Like: recipient@gmail.com): ")
         with open("db.json", "w") as f:
             db = {
-                "java": {"release": {"versions": []}, "snapshot": {"versions": []}},
-                "bedrock": {"release": {"versions": []}, "preview": {"versions": []}}
+                "config": {
+                    "mail_server": mail_server,
+                    "mail_port": mail_port,
+                    "mail_username": mail_username,
+                    "mail_password": mail_password,
+                    "mail_from": mail_from,
+                    "mail_to": mail_to,
+                },
+                "java": {
+                    "release": {"versions": []},
+                    "snapshot": {"versions": []},
+                },
+                "bedrock": {
+                    "release": {"versions": []},
+                    "preview": {"versions": []},
+                },
             }
             f.write(json.dumps(db))
             return db
